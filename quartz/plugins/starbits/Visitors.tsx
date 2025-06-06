fdb6e7d51030ab25beb2ae028e6d692fa43b53e6
Ben Cuan <github@bencuan.me>
2025-06-06 02:10:21 -0700

    fix error

--- quartz/plugins/starbits/Visitors.tsx
+++ quartz/plugins/starbits/Visitors.tsx
@@ -1,19 +1,8 @@
 const Visitors = () => {
-  function updateVisitors() {
-    fetch("https://api.bencuan.me/update-claps", {
-      method: "POST",
-    })
-    const container: Element | null = document.querySelector(".visitors")
-    if (!container) {
-      return
-    }
-    const visitors = parseInt(container!.innerHTML.split(" ")[1])
-    container.innerHTML = `<span className="including-you">${visitors + 1} people were here including you :)</span>`
-  }
-
+  // Actual script located in quartz/components/scripts/homepage.inline.ts
+  // TODO this is probably unnecessary to keep around as a starbit
   return (
     <>
-      <script>{updateVisitors.toString()}</script>
       <div className="visitors" />
     </>
   )
